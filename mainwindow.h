069e5fb8623fdf89d9b4ae03216c70fc64dc4c15
xdexst <rnoy10303@gmail.com>
2024-03-22 21:26:35 +0400

    git commit -m

---
+++ mainwindow.h
@@ -0,0 +1,24 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QLabel>
+#include <QTimer>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+
+private slots:
+    void updateTimer();
+
+private:
+    QLabel *label;
+    QTimer *timer;
+    int seconds;
+};
+
+#endif // MAINWINDOW_H
